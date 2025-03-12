15b55b8fe6afd5bef447e36bd4fd28aefe48500e
Terry <644052732@qq.com>
2025-03-12 20:50:25 +0800

    refactor: simplified the logic of get locks data using get_storage_at

--- query/src/bin/get_locks_data.rs
+++ query/src/bin/get_locks_data.rs
@@ -1,7 +1,7 @@
 use alloy::{
     network::EthereumWallet,
     node_bindings::Anvil,
-    primitives::{Address, U256, keccak256},
+    primitives::{Address, U64, U256, keccak256},
     providers::{Provider, ProviderBuilder},
     signers::local::PrivateKeySigner,
     sol,
@@ -69,28 +69,55 @@ async fn main() -> Result<()> {
         let index = U256::from(i);
 
         // 计算第i个元素第一个slot的位置
-        let slot_i = lock_info_slot_data + (index * U256::from(2));
+        let slot_i_1 = lock_info_slot_data + (index * U256::from(2));
+        // 第i个元素第二个slot的位置
+        let slot_i_2 = slot_i_1 + U256::from(1);
 
+        // let slot_i_1_data = http_provider
+        //     .get_storage_at(*es_rnt_address, slot_i_1)
+        //     .await?;
+        // let slot_i_1_bytes: [u8; 32] = slot_i_1_data
+        //     .to_be_bytes_vec()
+        //     .try_into()
+        //     .expect("failed to convert index i, slot 1 to [u8; 32]");
+        // // let slot_i_1_bytes: [u8; 32] = (*slot_i_1_data).into();
+        // // 拿到数据拆分出 address user, uint64 startTime
+        // let user_addr = Address::from_slice(&slot_i_1_bytes[12..32]);
+        // // let start_time_bytes: [u8; 8] = slot_i_1_bytes[4..12].try_into()?;
+        // // let start_time = u64::from_be_bytes(start_time_bytes);
+        // let start_time = U64::from_be_slice(&slot_i_1_bytes[4..12]);
+        //
+        // let slot_i_2_data = http_provider
+        //     .get_storage_at(*es_rnt_address, slot_i_2)
+        //     .await?;
+        //
+        // let slot_i_2_bytes: [u8; 32] = slot_i_2_data
+        //     .to_be_bytes_vec()
+        //     .try_into()
+        //     .expect("failed to convert index i, slot 2 to [u8; 32]");
+        // let amount = U256::from_be_bytes(slot_i_2_bytes);
+
+        // 简化一些步骤
+        // 获取改元素第一个slot的数据
         let slot_i_1_data = http_provider
-            .get_storage_at(*es_rnt_address, slot_i)
+            .get_storage_at(*es_rnt_address, slot_i_1)
             .await?;
-
-        let slot_i_1_bytes: [u8; 32] = slot_i_1_data
-            .to_be_bytes_vec()
-            .try_into()
-            .expect("failed to convert index i, slot 1 to [u8; 32]");
-        // let slot_i_1_bytes: [u8; 32] = (*slot_i_1_data).into();
-        // 拿到数据拆分出 address user, uint64 startTime
+        // 转换为bytes 即 Vec<u8> 无需进一步转换为bytes32 即 [u8; 32]
+        let slot_i_1_bytes = slot_i_1_data.to_be_bytes_vec();
+        // 解析地址 20字节
         let user_addr = Address::from_slice(&slot_i_1_bytes[12..32]);
-        let start_time_bytes: [u8; 8] = slot_i_1_bytes[4..12].try_into()?;
-        let start_time = u64::from_be_bytes(start_time_bytes);
-
-        // 第二个slot的位置
-        let slot2 = slot_i + U256::from(1);
-        let slot2_data = http_provider.get_storage_at(*es_rnt_address, slot2).await?;
-
-        let slot2_data_bytes: [u8; 32] = slot2_data.to_be_bytes_vec().try_into().expect("failed");
-        let amount = U256::from_be_bytes(slot2_data_bytes);
+        // 解析startTime 8字节
+        let start_time = U64::from_be_slice(&slot_i_1_bytes[4..12]);
+        // 获取该元素第二个slot的数据
+        let slot_i_2_data = http_provider
+            .get_storage_at(*es_rnt_address, slot_i_2)
+            .await?;
+        // 转换为bytes
+        let slot_i_2_bytes = slot_i_2_data.to_be_bytes_vec();
+        // 解析出 amount uint256, 32个字节，这里get_storage_at返回的数据刚好为
+        // uint256 类型大小，而且alloy返回的数据刚好使用U256表示
+        // 所以巧合导致slot_i_data 刚好等价于 amount，包括数据类型
+        let amount = U256::from_be_slice(&slot_i_2_bytes);
 
         println!(
             "locks[{}]: user: {:?}, startTime: {:?}, amount: {:?}",
