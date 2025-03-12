7592ae2b9779c9dff7e5a48bdb12a5b34d631e1a
Terry <644052732@qq.com>
2025-03-12 14:37:43 +0800

    feat: add example, use get_storage_at to get the esRNT locks data, add execution log

---
+++ query/src/bin/get_locks_data.rs
@@ -0,0 +1,102 @@
+use alloy::{
+    network::EthereumWallet,
+    node_bindings::Anvil,
+    primitives::{Address, U256, keccak256},
+    providers::{Provider, ProviderBuilder},
+    signers::local::PrivateKeySigner,
+    sol,
+};
+use eyre::Result;
+
+sol! {
+    #[allow(missing_docs)]
+    // solc v0.8.26; solc Counter.sol --viz-ir --optimize --bin
+    #[sol(rpc, bytecode="608060405234801561000f575f80fd5b505f5b600b8110156100e6575f60405180606001604052808360016100349190610100565b6001600160a01b031681526020018361004e426002610119565b6100589190610130565b6001600160401b03168152602001610071846001610100565b61008390670de0b6b3a7640000610119565b90528154600181810184555f93845260209384902083516002909302018054948401516001600160401b0316600160a01b026001600160e01b03199095166001600160a01b03909316929092179390931781556040909101519082015501610012565b50610143565b634e487b7160e01b5f52601160045260245ffd5b80820180821115610113576101136100ec565b92915050565b8082028115828204841417610113576101136100ec565b81810381811115610113576101136100ec565b603e8061014f5f395ff3fe60806040525f80fdfea264697066735822122061c6be102b852df2191c9d69e810dfdc06e005d40a4ee6dd27462a263b7c273b64736f6c634300081a0033")]
+    contract esRNT {
+        struct LockInfo {
+            address user;
+            uint64 startTime;
+            uint256 amount;
+        }
+
+        LockInfo[] private _locks;
+
+        constructor() {
+            for (uint256 i = 0; i < 11; i++) {
+                _locks.push(LockInfo(address(uint160(i + 1)), uint64(block.timestamp * 2 - i), 1e18 * (i + 1)));
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Spin up a local Anvil node.
+    // Ensure `anvil` is available in $PATH.
+    let anvil = Anvil::new().block_time(1).try_spawn()?;
+    let pk: PrivateKeySigner = anvil.keys()[0].clone().into();
+    let wallet = EthereumWallet::new(pk);
+
+    // Create a WebSocket provider.
+    // let ws = WsConnect::new(anvil.ws_endpoint());
+    // let provider = ProviderBuilder::new().wallet(wallet).on_ws(ws).await?;
+    // use http client
+    let http_provider = ProviderBuilder::new()
+        .wallet(wallet)
+        .on_http(anvil.endpoint_url());
+
+    let contract = esRNT::deploy(http_provider.clone()).await?;
+
+    println!("Deployed esRNT contract at: {}", contract.address());
+
+    let es_rnt_address = contract.address();
+    let lock_info_slot = U256::from(0);
+
+    // 默认在latest block上调用, .block() 可以设置一个区块
+    let length = http_provider
+        .get_storage_at(*es_rnt_address, lock_info_slot)
+        .await?;
+    println!(
+        "get the lock info length: {:?} from the lockInfo slot 0",
+        length
+    );
+
+    let lock_info_slot_data =
+        U256::from_be_bytes(keccak256(lock_info_slot.to_be_bytes_vec()).into());
+
+    for i in 0..length.to::<u64>() {
+        // 元素下标
+        let index = U256::from(i);
+
+        // 计算第i个元素第一个slot的位置
+        let slot_i = lock_info_slot_data + (index * U256::from(2));
+
+        let slot_i_1_data = http_provider
+            .get_storage_at(*es_rnt_address, slot_i)
+            .await?;
+
+        let slot_i_1_bytes: [u8; 32] = slot_i_1_data
+            .to_be_bytes_vec()
+            .try_into()
+            .expect("failed to convert index i, slot 1 to [u8; 32]");
+        // let slot_i_1_bytes: [u8; 32] = (*slot_i_1_data).into();
+        // 拿到数据拆分出 address user, uint64 startTime
+        let user_addr = Address::from_slice(&slot_i_1_bytes[12..32]);
+        let start_time_bytes: [u8; 8] = slot_i_1_bytes[4..12].try_into()?;
+        let start_time = u64::from_be_bytes(start_time_bytes);
+
+        // 第二个slot的位置
+        let slot2 = slot_i + U256::from(1);
+        let slot2_data = http_provider.get_storage_at(*es_rnt_address, slot2).await?;
+
+        let slot2_data_bytes: [u8; 32] = slot2_data.to_be_bytes_vec().try_into().expect("failed");
+        let amount = U256::from_be_bytes(slot2_data_bytes);
+
+        println!(
+            "locks[{}]: user: {:?}, startTime: {:?}, amount: {:?}",
+            i, user_addr, start_time, amount
+        );
+    }
+
+    Ok(())
+}
